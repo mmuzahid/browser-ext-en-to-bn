876826a5f9756cb42d9ddb758231b4c37de1fe04
muzahidengr <muzahid.engr@gmail.com>
2014-10-25 11:19:39 +0600

    Alpha release 0.0.1

---
+++ extension.js
@@ -0,0 +1,84 @@
+  /************************************************************************************
+  This is my Page Code. The appAPI.ready() code block will be executed on every page load.
+  For more information please visit our docs site: http://docs.crossrider.com
+*************************************************************************************/
+
+appAPI.ready(function($) {
+
+    // Place your code here (you can also define new functions above this scope)
+    // The $ object is the extension's  object
+    
+    // This example detects a selection between 3 and 20 characters anywhere on a page
+    // and displays a popup with an option to search for the selected text using Google
+    appAPI.selectedText(function(text, event) {
+        // Removes previous #popupText elements
+        $(".popupTranslatedText").remove();
+        
+        appAPI.request.get({
+          url: "https://translate.google.com/translate_a/single?client=t&sl=en&tl=bn&hl=en&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=sw&ie=UTF-8&oe=UTF-8&prev=btn&rom=1&ssel=0&tsel=0&q="+text,
+          
+          onSuccess: function(response) {
+            var data = response.toString();
+            //process data
+            data = " &nbsp; &nbsp; '" + text.toString().trim() + "': " + data.substring(data.indexOf('"')+1, data.indexOf('"', data.indexOf('"') + 1));
+            console.log("postback succeeded with response: " + response);
+            // Adds the hidden popup to the page
+            $("<p>").attr({
+                // The class for the popup
+                class : 'popupTranslatedText',
+            }).html(data).css({
+                //'position' : 'absolute',
+                // Specify the location of the popup relative to the location of the selected text
+               /* 'top' : event.pageY > 40 ? event.pageY - 30 : event.pageY + 10,
+                'left' : event.pageX > 20 ? event.pageX - 10 : event.pageX + 10,
+                'background-color' : '#782525',
+                'color' : 'red'*/
+              
+                'position': 'fixed',
+                'top': '0px',
+                'right': '0px',
+                'width': '200px',
+                //'height': '100px',
+                //'text-align': 'center',
+                'line-height': '100px',
+                'z-index': '9999',
+                'background-color': '#fff',
+                'color' : 'blue',
+                'border': '8px solid #666',
+                '-webkit-border-radius': '30px',
+                '-moz-border-radius': '30px',
+                'border-radius': '30px',
+                '-webkit-box-shadow': '2px 2px 4px #888',
+                '-moz-box-shadow': '2px 2px 4px #888',
+                'box-shadow': '2px 2px 4px #888'
+                    
+    
+            // Slowly fade the popup in
+            }).hide().appendTo('body').fadeIn('slow');
+            
+            // Slowly hide the popup when another location in the page is clicked
+            // (Does not prevent the Google search link from being clicked)
+            $(document).mousedown(function(){
+                $(".popupTranslatedText").fadeOut("slow");
+            }); 
+          },
+          onFailure: function(httpCode) {
+            console.log("postback failure: " + httpCode);
+            // Slowly hide the popup when another location in the page is clicked
+            // (Does not prevent the Google search link from being clicked)
+            $(document).mousedown(function(){
+                $(".popupTranslatedText").fadeOut("slow");
+            }); 
+          },
+
+          contentType: 'application/text'
+        });
+        
+  
+    }, {
+        // Min selection length to trigger callback
+        minlength: 2,
+        // Max selection length to trigger callback
+        maxlength: 25
+    });
+});
